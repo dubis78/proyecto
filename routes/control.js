17a93d0df610a32ed734c9f86cce0bafefeb5bcb
dubis78 <dubis85201@hotmail.com>
2021-01-18 20:57:16 -0500

    add more services

--- routes/control.js
+++ routes/control.js
@@ -16,7 +16,7 @@ router.get("/tipos-linea", async(req,res) =>{
     const [rows]=await cnn_mysql.execute(`SELECT * FROM TIPO_LINEA`);
     res.json(rows);
     
-  } catch (error) {
+  } catch (e) {
     res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
   }
 });
@@ -26,7 +26,7 @@ router.get("/vehiculos", async(req,res) =>{
       const [rows]=await cnn_mysql.execute(`SELECT * FROM VEHICULOS`);
       res.json(rows);
       
-    } catch (error) {
+    } catch (e) {
       res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
     }
   });
@@ -59,7 +59,7 @@ router.get("/check-linea", async(req,res) =>{
         res.send(`<h1>Tabla TIPO_LINEA no cumple! tiene ${rows[0]['COUNT(*)']} registros</h1>`);
       }
     } 
-  } catch (error) {
+  } catch (e) {
     res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
   }
 });
@@ -75,10 +75,11 @@ router.get("/check-vehiculos", async(req,res) =>{
           res.send(`<h1>Tabla VEHICULOS no cumple! tiene ${rows[0]['COUNT(*)']} registros</h1>`);
         }
       }        
-    } catch (error) {
+    } catch (e) {
       res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
     }
   });
+
 
 
 router.get("/max-min-vehiculos", async(req,res) =>{
@@ -87,21 +88,178 @@ router.get("/max-min-vehiculos", async(req,res) =>{
       if(rows[0]){
           res.json(rows[0]);
         }        
-    } catch (error) {
+    } catch (e) {
       res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
     }
   });
 
 
-  router.get("/tot-line-per-brand", async(req,res) =>{
-    try {
-      const [rows]=await cnn_mysql.execute(`SELECT TIPO_LINEA.DESC_LINEA, TIPO_MARCA.DESC_MARCA, TIPO_LINEA.ID_MARCA,COUNT(TIPO_LINEA.ID_MARCA) AS 'TOTAL-LINEAS-POR-MARCA' FROM TIPO_LINEA INNER JOIN TIPO_MARCA ON TIPO_MARCA.ID_MARCA = TIPO_LINEA.ID_MARCA GROUP BY TIPO_LINEA.ID_MARCA`);
-      if(rows[0]){
-          res.json(rows);
-        }        
-    } catch (error) {
-      res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
-    }
-  });
+
+router.get("/tot-line-per-brand", async(req,res) =>{
+  try {
+    const [rows]=await cnn_mysql.execute(`SELECT TIPO_LINEA.DESC_LINEA, TIPO_MARCA.DESC_MARCA, TIPO_LINEA.ID_MARCA,COUNT(TIPO_LINEA.ID_MARCA) AS 'TOTAL-LINEAS-POR-MARCA' FROM TIPO_LINEA INNER JOIN TIPO_MARCA ON TIPO_MARCA.ID_MARCA = TIPO_LINEA.ID_MARCA GROUP BY TIPO_LINEA.ID_MARCA`);
+    if(rows[0]){
+        res.json(rows);
+      }        
+  } catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+});
+
+
+
+router.get("/rang-date-exp", async(req,res) =>{
+  try {
+    const {start,end} = req.query;
+    console.log(`SELECT * FROM 'VEHICULOS' WHERE FECHA_VEN_SEGURO BETWEEN '${start}' AND '${end}'`);
+    const [rows]=await cnn_mysql.execute(`SELECT * FROM VEHICULOS WHERE FECHA_VEN_SEGURO BETWEEN ? AND ?`,[start,end]);
+    if(rows[0]){
+        res.json(rows);
+      }        
+  } catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+});
+
+
+
+router.get("/rang-mod", async(req,res) =>{
+  try {
+    const {start,end} = req.query;
+    const [rows]=await cnn_mysql.execute(`SELECT * FROM VEHICULOS WHERE MODELO BETWEEN '${start}' AND '${end}'`);
+    if(rows[0]){
+        res.json(rows);
+      }        
+  } catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+}); 
+
+
+
+router.patch("/update-linea/:id", async(req,res) =>{
+  try {
+    if (Object.keys(req.body).length > 0) { //Object.keys retorna un array de los "indices" de los elementos de un object o array
+      const id = req.params.id
+      let SQL = 'UPDATE TIPO_LINEA SET '
+      const params = []
+
+      for (const elment in req.body) {
+          SQL += `${elment} = ?, `
+          params.push(req.body[elment])
+      }
+      SQL = SQL.slice(0, -2)
+      SQL += ` WHERE ID_LINEA = ?`
+      params.push(id)
+      console.log(SQL, params)
+      let [rows] = await cnn_mysql.execute(SQL, params)
+      
+      if (rows.affectedRows > 0) {
+          [rows] = await cnn_mysql.query(`SELECT * FROM TIPO_LINEA WHERE ID_LINEA = ?`, [id])
+          res.json(rows[0])
+      }else{
+          res.json({})
+      }
+  } 
+    else {
+      res.status(401).json({ message: 'No existe campos a modificar' })
+    }     
+  } 
+  catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+}); 
+
+router.patch("/update-marca/:id", async(req,res) =>{
+  try {
+    if (Object.keys(req.body).length > 0) { //Object.keys retorna un array de los "indices" de los elementos de un object o array
+      const id = req.params.id
+      let SQL = 'UPDATE TIPO_MARCA SET '
+      const params = []
+
+      for (const elment in req.body) {
+          SQL += `${elment} = ?, `
+          params.push(req.body[elment])
+      }
+      SQL = SQL.slice(0, -2)
+      SQL += ` WHERE ID_MARCA = ?`
+      params.push(id)
+      console.log(SQL, params)
+      let [rows] = await cnn_mysql.execute(SQL, params)
+      
+      if (rows.affectedRows > 0) {
+          [rows] = await cnn_mysql.query(`SELECT * FROM TIPO_MARCA WHERE ID_MARCA = ?`, [id])
+          res.json(rows[0])
+      }else{
+          res.json({})
+      }
+  } 
+    else {
+      res.status(401).json({ message: 'No existe campos a modificar' })
+    }     
+  } 
+  catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+}); 
+  
+router.patch("/update-vehiculo/:id", async(req,res) =>{
+  try {
+    if (Object.keys(req.body).length > 0) { //Object.keys retorna un array de los "indices" de los elementos de un object o array
+      const id = req.params.id;
+      let SQL = 'UPDATE VEHICULOS SET '
+      const params = []
+
+      for (const elment in req.body) {
+          SQL += `${elment} = ?, `
+          params.push(req.body[elment])
+      }
+      SQL = SQL.slice(0, -2)
+      SQL += ` WHERE NRO_PLACA = ?`
+      params.push(id)
+      console.log(SQL, params)
+      let [rows] = await cnn_mysql.execute(SQL, params)
+      
+      if (rows.affectedRows > 0) {
+          [rows] = await cnn_mysql.query(`SELECT * FROM VEHICULOS WHERE NRO_PLACA = ?`, [id])
+          res.json(rows[0])
+      }else{
+          res.json({})
+      }
+  } 
+    else {
+      res.status(401).json({ message: 'No existe campos a modificar' })
+    }     
+  } 
+  catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+}); 
+
+
+
+router.post("/new-marca", async(req,res) =>{
+  try {
+    const {ID_MARCA,DESC_MARCA,ACTIVO} = req.body;
+    const [rows]=await cnn_mysql.execute(`INSERT INTO TIPO_MARCA (ID_MARCA, DESC_MARCA, ACTIVO) VALUES (?, ?, ?)`,[ID_MARCA,DESC_MARCA,ACTIVO]);
+    res.json({message: `Nueva marca agregada con id= ${rows.insertId}` });
+             
+  } catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+}); 
+
+router.delete("/marca/:id", async(req,res) =>{
+  try {
+    const id = req.params.id
+    const [rows]=await cnn_mysql.execute(`DELETE FROM TIPO_MARCA WHERE ID_MARCA = ?`,[id]);
+    res.json({message: `Registro con id= ${id} eliminado` });
+             
+  } catch (e) {
+    res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+  }
+}); 
+
+
 
 module.exports= router;
